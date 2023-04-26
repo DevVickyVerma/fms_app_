7bf2ab7fe5b3e77ef0f1afeb750c4f2235fd8fca
DevVickyVerma <vicky.credentia@gmail.com>
2023-04-27 00:35:59 +0530

    Dashboard and footer fix

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {Link} from "react-router-dom"
 import{Row,Col} from "react-bootstrap"
 const Footer = () => {
+
+	const currentYear = new Date().getFullYear();
+
 return(
 <div className="footer">
 			<div className="container">
 				<Row className="align-items-center flex-row-reverse">
 					<Col className="text-center" sm={12} md={12} lg={12}>
-						 Copyright © 2022 <Link to="#">Zenax</Link>. Designed with <span className="fa fa-heart text-danger"></span> by <Link to="#"> Spruko </Link> All rights reserved
+						 Copyright © {currentYear} Credentia. All rights reserved
 					</Col>
 				</Row>
 			</div>
