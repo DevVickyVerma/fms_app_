7903a5b97566e533fe1c55aa1223939555f2908f
DevVickyVerma <vicky.credentia@gmail.com>
2023-05-10 12:09:10 +0530

    d2

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -64,10 +64,10 @@ export function Header() {
         } else {
           ErrorAlert("Unknown error occurred");
         }
-        setTimeout(() => {
-          window.location.replace("/");
-          localStorage.clear();
-        }, 500);
+        // setTimeout(() => {
+        //   window.location.replace("/");
+        //   localStorage.clear();
+        // }, 500);
       }
     };
 
