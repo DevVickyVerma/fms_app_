a7bbb60c78a5116bfab196707a3f33a5c50d5b50
DevVickyVerma <vicky.credentia@gmail.com>
2023-08-10 15:13:22 +0530

    dataslice

--- src/Redux/dataSlice.js
+++ src/Redux/dataSlice.js
@@ -1,4 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { Slide, toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 // Define the initial state
 const initialState = {
@@ -6,7 +9,25 @@ const initialState = {
   loading: false,
   error: null,
 };
-
+const SuccessToast = (message) => {
+  toast.success(message, {
+    autoClose: 1000,
+    position: toast.POSITION.TOP_RIGHT,
+    hideProgressBar: true,
+    transition: Slide,
+    autoClose: 1000,
+    theme: "colored", // Set the duration in milliseconds (e.g., 3000ms = 3 seconds)
+  });
+};
+const ErrorToast = (message) => {
+  toast.error(message, {
+    position: toast.POSITION.TOP_RIGHT,
+    hideProgressBar: true,
+    transition: Slide,
+    autoClose: 1000,
+    theme: "colored", // Set the duration in milliseconds (e.g., 5000ms = 5 seconds)
+  });
+};
 // Create an asynchronous thunk to fetch the API data
 const baseUrl = process.env.REACT_APP_BASE_URL;
 export const fetchData = createAsyncThunk("data/fetchData", async (_, thunkAPI) => {
@@ -17,25 +38,56 @@ export const fetchData = createAsyncThunk("data/fetchData", async (_, thunkAPI) 
   }
 
   try {
-    const response = await fetch(`${baseUrl}/detail`, {
+    const response = await axios.get(`${baseUrl}/detail`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
-    const data = await response.json();
-    if(data){
+  
+    const data = response.data;
+  
+    if (data) {
       localStorage.setItem("superiorId", data?.data?.superiorId);
       localStorage.setItem("superiorRole", data?.data?.superiorRole);
       localStorage.setItem("role", data?.data?.role);
+    }
   
-  
-  }
-    
- 
     // Check the structure of the data
     return data.data;
   } catch (error) {
-    throw new Error("Failed to fetch data from API.");
+    if (axios.isAxiosError(error)) {
+      const { response } = error;
+  
+      if (response) {
+        if (response.status === 401) {
+          // Unauthorized access
+          // navigate("/login");
+          ErrorToast("Invalid access token");
+          localStorage.clear();
+        } else if (response.status === 403) {
+          // Forbidden
+          // navigate("/errorpage403");
+        } else if (response.data && response.data.message) {
+          // Other error message
+          const errorMessage = Array.isArray(response.data.message)
+            ? response.data.message.join(" ")
+            : response.data.message;
+  
+          if (errorMessage) {
+            ErrorToast(errorMessage);
+          }
+        } else {
+          // Unknown error
+          ErrorToast("An error occurred.");
+        }
+      } else {
+        // Network error or other issues
+        ErrorToast("Failed to fetch data from API.");
+      }
+    } else {
+      // Non-Axios error
+      throw new Error("An error occurred.");
+    }
   }
 });
 
