34f309df7e127377e7b2333fd7cd6d7c7902c3c7
DevVickyVerma1 <jitendra.credentia@gmail.com>
2023-09-01 13:02:28 +0530

    site graph's response has been changed

--- src/components/Dashboard/dashTopSection/DashboardSiteGraph.jsx
+++ src/components/Dashboard/dashTopSection/DashboardSiteGraph.jsx
@@ -18,7 +18,7 @@ const DashboardSiteGraph = ({ getSiteStats, setGetSiteStats }) => {
       ? getSiteStats?.data?.stock_alert
       : [];
 
-  // console.log("my alert data", stockAlertData);
+  console.log("my alert data", stockAlertData);
 
   const generateGradientStops = (color) => [
     {
@@ -45,9 +45,6 @@ const DashboardSiteGraph = ({ getSiteStats, setGetSiteStats }) => {
     setSelectedDateIndex(dateIndex);
   };
 
-  // const selectedDateData = stockAlertData[selectedDateIndex];
-  // const selectedDateData = stockAlertData[selectedDateIndex];
-
   return (
     <div>
       <Row>
@@ -55,62 +52,53 @@ const DashboardSiteGraph = ({ getSiteStats, setGetSiteStats }) => {
           <Card>
             <Card.Header>
               <Card.Title as="h3" className="gap-3 d-flex flex-wrap">
-                {getSiteStats?.data?.stock_alert?.[0]?.map(
-                  (tankDate, index) => (
-                    <>
-                      <Box
-                        borderRadius={"5px"}
-                        bgcolor={
-                          selectedDateIndex === index ? "purple" : "#5444c1"
-                        }
-                        px={"20px"}
-                        py={"15px"}
-                        color={"white"}
-                        // minWidth={"150px"}
-                        onClick={() => handleDateButtonClick(index)}
-                        key={index}
-                        sx={{
-                          ":hover": {
-                            backgroundColor: "purple", // Change background color on hover
-                            cursor: "pointer", // Change cursor to pointer on hover
-                          },
-                        }}
+                {getSiteStats?.data?.dates?.map((tankDate, index) => (
+                  <>
+                    <Box
+                      borderRadius={"5px"}
+                      bgcolor={
+                        selectedDateIndex === index ? "purple" : "#5444c1"
+                      }
+                      px={"20px"}
+                      py={"15px"}
+                      color={"white"}
+                      // minWidth={"150px"}
+                      onClick={() => handleDateButtonClick(index)}
+                      key={index}
+                      sx={{
+                        ":hover": {
+                          backgroundColor: "purple", // Change background color on hover
+                          cursor: "pointer", // Change cursor to pointer on hover
+                        },
+                      }}
+                    >
+                      <Typography
+                        display={"flex"}
+                        gap={"5px"}
+                        alignItems={"center"}
+                        mb={"5px"}
                       >
-                        <Typography
-                          display={"flex"}
-                          gap={"5px"}
-                          alignItems={"center"}
-                          mb={"5px"}
-                        >
-                          {tankDate?.date}
-                        </Typography>
-                      </Box>
-                    </>
-                  )
-                )}
+                        {tankDate}
+                      </Typography>
+                    </Box>
+                  </>
+                ))}
               </Card.Title>
             </Card.Header>
 
             <Card.Body>
               <Row>
-                {stockAlertData?.map((tankData, index) => (
+                {/* {stockAlertData?.map((tankData, index) => (
                   <Col lg={3} xl={3} md={6} sm={12}>
                     <div
                       key={index}
                       className="m-4"
                       style={{
-                        // boxShadow: "0px 10px 10px -5px rgba(0,0,0,0.5)",
-                        // boxShadow: "0 3px 9px 0 rgba(169, 184, 200, 0.15)",
                         boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
                         background: "white",
-                        // borderRadius: "5px",
                         padding: "10px",
                         borderRadius: "10px",
                       }}
-                      //    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-                      // background: white;
-                      // border-radius: 5px;
-                      // padding: 10px;
                     >
                       <div>
                         <p className="text-center">
@@ -124,11 +112,11 @@ const DashboardSiteGraph = ({ getSiteStats, setGetSiteStats }) => {
                       </div>
                       <LiquidFillGauge
                         style={{ margin: "0 auto" }}
-                        width={180} // Adjust the width and height as needed
+                        width={180}
                         height={180}
                         value={
                           tankData?.[selectedDateIndex]?.fuel_left_percentage
-                        } // Use the relevant data point here
+                        }
                         percent="%"
                         textSize={1}
                         textOffsetX={0}
@@ -142,7 +130,6 @@ const DashboardSiteGraph = ({ getSiteStats, setGetSiteStats }) => {
                               }}
                             >
                               <tspan>
-                                {/* <tspan>{props?.value?.toFixed(2)}</tspan> */}
                                 {Number(props?.value).toFixed(2)}
                                 <tspan>{props?.percent}</tspan>
                               </tspan>
@@ -156,30 +143,26 @@ const DashboardSiteGraph = ({ getSiteStats, setGetSiteStats }) => {
                         gradient
                         gradientStops={generateGradientStops(
                           tankData?.[selectedDateIndex]?.bg_color
-                        )} // Use the relevant color property
+                        )}
                         circleStyle={{
                           fill: tankData?.[selectedDateIndex]?.bg_color,
                         }}
                         waveStyle={{
-                          fill: tankData?.[selectedDateIndex]?.bg_color, // Use the relevant color property
+                          fill: tankData?.[selectedDateIndex]?.bg_color,
                         }}
                         textStyle={{
-                          fill: tankData?.[selectedDateIndex]?.font_color, // Use the relevant color property
+                          fill: tankData?.[selectedDateIndex]?.font_color,
                           fontFamily: "Arial",
                         }}
                         waveTextStyle={{
-                          fill: tankData?.[selectedDateIndex]?.font_color, // Use the relevant color property
+                          fill: tankData?.[selectedDateIndex]?.font_color,
                           fontFamily: "Arial",
                         }}
-                        // onClick={() => {
-                        //   // Handle click event if needed
-                        // }}
                       />
-                      {/* Render other information as needed */}
+
                       <div
                         className="pt-3 mt-3"
                         style={{
-                          // borderTop: "1px solid #2f3640",
                           lineHeight: 1,
                         }}
                       >
@@ -255,6 +238,205 @@ const DashboardSiteGraph = ({ getSiteStats, setGetSiteStats }) => {
                       </div>
                     </div>
                   </Col>
+                ))} */}
+                {Object?.keys(stockAlertData)?.map((tankName) => (
+                  <>
+                    {/* <div key={tankName}>
+                      <h2>Tank: {tankName}</h2>
+                      <ul>
+                        {stockAlertData[tankName][selectedDateIndex].font_color}
+                        {stockAlertData[tankName][selectedDateIndex].fuel_left}
+                      </ul>
+                    </div> */}
+                    <Col lg={3} xl={3} md={6} sm={12}>
+                      <div
+                        key={tankName}
+                        className="m-4"
+                        style={{
+                          boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+                          background: "white",
+                          padding: "10px",
+                          borderRadius: "10px",
+                        }}
+                      >
+                        <div>
+                          <p className="text-center">
+                            <strong
+                              className="mb-2  text-dark font-weight-bold"
+                              style={{ fontSize: "14px", fontWeight: "bold" }}
+                            >
+                              {/* {tankName?.[selectedDateIndex]?.tank_name} */}
+                              {
+                                stockAlertData?.[tankName]?.[selectedDateIndex]
+                                  ?.tank_name
+                              }
+                            </strong>
+                          </p>
+                        </div>
+                        <LiquidFillGauge
+                          style={{ margin: "0 auto" }}
+                          width={180}
+                          height={180}
+                          value={
+                            stockAlertData?.[tankName]?.[selectedDateIndex]
+                              ?.fuel_left_percentage
+                          }
+                          percent="%"
+                          textSize={1}
+                          textOffsetX={0}
+                          textOffsetY={0}
+                          textRenderer={(props) => (
+                            <>
+                              <tspan
+                                style={{
+                                  display: "flex",
+                                  flexDirection: "column",
+                                }}
+                              >
+                                <tspan>
+                                  {Number(props?.value).toFixed(2)}
+                                  <tspan>{props?.percent}</tspan>
+                                </tspan>
+                              </tspan>
+                            </>
+                          )}
+                          riseAnimation
+                          waveAnimation
+                          waveFrequency={2}
+                          waveAmplitude={1}
+                          gradient
+                          gradientStops={generateGradientStops(
+                            stockAlertData?.[tankName]?.[selectedDateIndex]
+                              ?.bg_color
+                          )}
+                          circleStyle={{
+                            fill: stockAlertData?.[tankName]?.[
+                              selectedDateIndex
+                            ]?.bg_color,
+                          }}
+                          waveStyle={{
+                            fill: stockAlertData?.[tankName]?.bg_color,
+                          }}
+                          textStyle={{
+                            fill: stockAlertData?.[tankName]?.[
+                              selectedDateIndex
+                            ]?.font_color,
+                            fontFamily: "Arial",
+                          }}
+                          waveTextStyle={{
+                            fill: stockAlertData?.[tankName]?.[
+                              selectedDateIndex
+                            ]?.font_color,
+                            fontFamily: "Arial",
+                          }}
+                        />
+
+                        <div
+                          className="pt-3 mt-3"
+                          style={{
+                            lineHeight: 1,
+                          }}
+                        >
+                          <p>
+                            <strong className="mb-0 fs-8 text-dark">
+                              <span
+                                style={{ fontSize: "14px", fontWeight: "bold" }}
+                              >
+                                Capacity:
+                              </span>
+                              {
+                                stockAlertData?.[tankName]?.[selectedDateIndex]
+                                  ?.capacity
+                              }
+                              ℓ{" "}
+                              <span>
+                                <OverlayTrigger
+                                  placement="top"
+                                  overlay={
+                                    <Tooltip>
+                                      {" "}
+                                      Average Sale :{" "}
+                                      {
+                                        stockAlertData?.[tankName]?.[
+                                          selectedDateIndex
+                                        ]?.average_sale
+                                      }
+                                      ℓ{" "}
+                                    </Tooltip>
+                                  }
+                                >
+                                  <i
+                                    class="fa fa-info-circle"
+                                    aria-hidden="true"
+                                  ></i>
+                                </OverlayTrigger>
+                              </span>
+                            </strong>
+                          </p>
+                          <p>
+                            <strong className="mb-0 fs-8 text-dark">
+                              <span
+                                style={{ fontSize: "14px", fontWeight: "bold" }}
+                              >
+                                Ullage:
+                              </span>
+                              {
+                                stockAlertData?.[tankName]?.[selectedDateIndex]
+                                  ?.ullage
+                              }
+                              {/* {Number(
+                                stockAlertData?.[tankName]?.[selectedDateIndex]
+                                  ?.ullage
+                              ).toFixed(2)} */}
+                              ℓ |{" "}
+                              {
+                                stockAlertData?.[tankName]?.[selectedDateIndex]
+                                  ?.ullage_percentage
+                              }
+                              %
+                            </strong>
+                          </p>
+                          <p>
+                            <strong className="mb-0 fs-8 text-dark">
+                              <span
+                                style={{ fontSize: "14px", fontWeight: "bold" }}
+                              >
+                                Fuel:
+                              </span>
+                              {
+                                stockAlertData?.[tankName]?.[selectedDateIndex]
+                                  ?.fuel_left
+                              }
+                              ℓ
+                            </strong>
+                            <span
+                              className="mb-0 mx-2 fs-8  badge  "
+                              style={{
+                                backgroundColor:
+                                  stockAlertData?.[tankName]?.[
+                                    selectedDateIndex
+                                  ]?.bg_color || "gray",
+                                color:
+                                  stockAlertData?.[tankName]?.[
+                                    selectedDateIndex
+                                  ]?.font_color || "black",
+
+                                borderRadius: "4px",
+                              }}
+                            >
+                              {" "}
+                              {
+                                stockAlertData?.[tankName]?.[selectedDateIndex]
+                                  ?.days_left
+                              }{" "}
+                              Days
+                            </span>
+                          </p>
+                          <p></p>
+                        </div>
+                      </div>
+                    </Col>
+                  </>
                 ))}
               </Row>
             </Card.Body>
