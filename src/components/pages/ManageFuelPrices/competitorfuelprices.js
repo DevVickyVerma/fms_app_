75176e72781070526c664b19242ad1bc4b42fbcd
DevVickyVerma <vicky.credentia@gmail.com>
2023-09-08 16:38:08 +0530

    compititve price

--- src/components/pages/ManageFuelPrices/competitorfuelprices.js
+++ src/components/pages/ManageFuelPrices/competitorfuelprices.js
@@ -20,58 +20,34 @@ import { useNavigate } from "react-router-dom";
 import { Slide, toast } from "react-toastify";
 import withApi from "../../../Utils/ApiHelper";
 import { ErrorMessage, Field, Formik, useFormik } from "formik";
-import MiddayModal from "../../../data/Modal/MiddayModal";
-import CustomModal from "../../../data/Modal/MiddayModal";
+import { Collapse, Table } from "antd";
+
+const { Panel } = Collapse;
 
 const CompetitorFuelPrices = (props) => {
   const { apidata, error, getData, postData, SiteID, ReportDate, isLoading } =
     props;
 
-  // const [data, setData] = useState()
-  //   const [data, setData] = useState([]);
-  const [DeductionData, setDeductionData] = useState([]);
   const [editable, setis_editable] = useState();
-
   const [AddSiteData, setAddSiteData] = useState([]);
-  // const [selectedBusinessType, setSelectedBusinessType] = useState("");
-  // const [subTypes, setSubTypes] = useState([]);
   const [selectedClientId, setSelectedClientId] = useState("");
   const [selectedCompanyId, setSelectedCompanyId] = useState("");
   const [selectedDrsDate, setSelectedDrsDate] = useState("");
   const [selectedCompanyList, setSelectedCompanyList] = useState([]);
   const [selectedSiteList, setSelectedSiteList] = useState([]);
-  const [headingData, setheadingData] = useState([]);
+
   const [clientIDLocalStorage, setclientIDLocalStorage] = useState(
     localStorage.getItem("superiorId")
   );
-  const [isChecked, setIsChecked] = useState(false);
 
   const navigate = useNavigate();
-  const SuccessToast = (message) => {
-    toast.success(message, {
-      autoClose: 1000,
-      position: toast.POSITION.TOP_RIGHT,
-      hideProgressBar: true,
-      transition: Slide,
-      autoClose: 1000,
-      theme: "colored", // Set the duration in milliseconds (e.g., 3000ms = 3 seconds)
-    });
-  };
-  const ErrorToast = (message) => {
-    toast.error(message, {
-      position: toast.POSITION.TOP_RIGHT,
-      hideProgressBar: true,
-      transition: Slide,
-      autoClose: 1000,
-      theme: "colored", // Set the duration in milliseconds (e.g., 5000ms = 5 seconds)
-    });
-  };
 
   useEffect(() => {
     setclientIDLocalStorage(localStorage.getItem("superiorId"));
     handleFetchData();
     console.clear();
   }, []);
+
   const handleFetchData = async () => {
     try {
       const response = await getData("/client/commonlist");
@@ -89,10 +65,6 @@ const CompetitorFuelPrices = (props) => {
 
             setSelectedCompanyList([]);
 
-            // setShowButton(false);
-            console.log(clientId, "clientId");
-            console.log(AddSiteData, "AddSiteData");
-
             if (response?.data) {
               const selectedClient = response?.data?.data?.find(
                 (client) => client.id === clientId
@@ -108,6 +80,7 @@ const CompetitorFuelPrices = (props) => {
       console.error("API error:", error);
     }
   };
+  const [data, setData] = useState(null);
   const handleSubmit1 = async (values) => {
     setSelectedCompanyId(values.company_id);
     setSelectedDrsDate(values.start_date);
@@ -118,8 +91,6 @@ const CompetitorFuelPrices = (props) => {
       formData.append("client_id", values.client_id);
       formData.append("company_id", values.company_id);
 
-      // ...
-
       let clientIDCondition = "";
       if (localStorage.getItem("superiorRole") !== "Client") {
         clientIDCondition = `client_id=${values.client_id}&`;
@@ -127,180 +98,63 @@ const CompetitorFuelPrices = (props) => {
         clientIDCondition = `client_id=${clientIDLocalStorage}&`;
       }
       const response1 = await getData(
-        `site/fuel-price?${clientIDCondition}company_id=${values.company_id}&drs_date=${values.start_date}`
+        `site/competitor-price?${clientIDCondition}company_id=${values.company_id}&drs_date=${values.start_date}`
       );
 
       const { data } = response1;
       if (data) {
-        setheadingData(data?.data?.head_array);
         setData(data?.data);
         setis_editable(data?.data?.btn_clickable);
-        setIsChecked(data?.data?.notify_operator);
-
-        console.log("Data received from child:", "head_array");
       }
     } catch (error) {
       console.error("API error:", error);
     }
   };
 
-  const [data, setData] = useState();
-  const renderTableHeader = () => {
-    return (
-      <tr className="fuelprice-tr">
-        {data?.head_array.map((item, index) => (
-          <th key={index}>{item}</th>
-        ))}
-      </tr>
-    );
-  };
-
   const hadndleShowDate = () => {
     const inputDateElement = document.querySelector('input[type="date"]');
     inputDateElement.showPicker();
   };
-  const [modalOpen, setModalOpen] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [selectedItemDate, setSelectedItemDate] = useState();
-  const handleModalOpen = (item) => {
-    setSelectedItem(item); // Set the selected item
-    setModalOpen(true);
-  };
+  const extractFuelData = (site) => {
+    if (site.competitors && site.competitors.length > 0) {
+      const competitor = site.competitors[0];
+      const fuels = competitor.fuels[0] || {}; // Use the first element, or an empty object if not available
 
-  const handleModalClose = () => {
-    setModalOpen(false);
-  };
+      const time = fuels.time || "N/A";
+      const unleaded = fuels.price || "N/A";
+      const superUnleaded =
+        (competitor.fuels[1] && competitor.fuels[1].price) || "N/A";
+      const diesel =
+        (competitor.fuels[2] && competitor.fuels[2].price) || "N/A";
+      const superDiesel =
+        (competitor.fuels[3] && competitor.fuels[3].price) || "N/A";
+      const other = (competitor.fuels[4] && competitor.fuels[4].price) || "N/A";
 
-  const renderTableData = () => {
-    return data?.listing.map((item) => (
-      <tr className="fuelprice-tr" key={item.id}>
-        <td>
-          <span
-            className={
-              item?.link_clickable
-                ? "text-muted fs-15 fw-semibold text-center fuel-site-name"
-                : "text-muted fs-15 fw-semibold text-center"
-            }
-          >
-            {item?.site_name}
-          </span>
-        </td>
-        <td>
-          <span class="text-muted fs-15 fw-semibold text-center">
-            {item.time}
-          </span>
-        </td>
-
-        {item?.fuels.map((fuel, index) => (
-          <td key={index}>
-            {Array.isArray(fuel) ? (
-              <input type="text" className="table-input readonly" readOnly />
-            ) : (
-              <input
-                type="number"
-                step="0.001"
-                // className="table-input"
-                className={`table-input ${
-                  fuel?.status === "UP"
-                    ? "table-inputGreen"
-                    : fuel?.status === "DOWN"
-                    ? "table-inputRed"
-                    : ""
-                } ${!fuel?.is_editable ? "readonly" : ""}`}
-                value={fuel.price}
-                readOnly={!fuel?.is_editable}
-                id={fuel.id}
-                onChange={(e) => handleInputChange(e.target.id, e.target.value)}
-              />
-            )}
-          </td>
-        ))}
-      </tr>
-    ));
-  };
-
-  // console.log("data listing check",data?.listing);
-
-  const handleInputChange = (id, value) => {
-    const updatedData = {
-      ...data,
-      listing: data?.listing?.map((item) => ({
-        ...item,
-        fuels: item.fuels.map((fuel) =>
-          fuel.id === id ? { ...fuel, price: value } : fuel
-        ),
-      })),
-    };
-
-    setData(updatedData);
-  };
-
-  const handleSubmit = async (values) => {
-    try {
-      const formData = new FormData();
-
-      data?.listing?.forEach((item) => {
-        const siteId = item.id;
-
-        item.fuels.forEach((fuel) => {
-          if (!Array.isArray(fuel) && fuel.price !== undefined) {
-            const priceId = fuel.id;
-            const fieldKey = `fuels[${siteId}][${priceId}]`;
-            const timeKey = `time[${siteId}][${priceId}]`;
-            const fieldValue = fuel.price.toString();
-            const fieldtime = fuel.time;
-            formData.append(fieldKey, fieldValue);
-            formData.append(timeKey, fieldtime);
-          }
-        });
-      });
-
-      setSelectedItemDate(selectedDrsDate);
-      formData.append("notify_operator", isChecked);
-      formData.append("drs_date", selectedDrsDate);
-      formData.append("client_id", selectedClientId);
-      formData.append("company_id", selectedCompanyId);
-
-      const postDataUrl = "/site/fuel-price/update-midday";
-      // const navigatePath = "/business";
-
-      await postData(postDataUrl, formData); // Set the submission state to false after the API call is completed
-    } catch (error) {
-      console.log(error); // Set the submission state to false if an error occurs
-    }
-  };
-
-  const SendNotification = (event) => {
-    setIsChecked(event.target.checked);
-  };
-  const handleDataFromChild = async (dataFromChild) => {
-    try {
-      // Assuming you have the 'values' object constructed from 'dataFromChild'
-      const values = {
-        start_date: selectedDrsDate,
-        client_id: selectedClientId,
-        company_id: selectedCompanyId,
+      return {
+        time,
+        unleaded,
+        superUnleaded,
+        diesel,
+        superDiesel,
+        other,
       };
-
-      await handleSubmit1(values);
-
-      console.log(dataFromChild, "dataFromChild");
-    } catch (error) {
-      console.error("Error handling data from child:", error);
     }
+
+    // If no competitor data available, return default values
+    return {
+      time: "N/A",
+      unleaded: "N/A",
+      superUnleaded: "N/A",
+      diesel: "N/A",
+      superDiesel: "N/A",
+      other: "N/A",
+    };
   };
 
   return (
     <>
       {isLoading ? <Loaderimg /> : null}
       <>
-        <CustomModal
-          open={modalOpen}
-          onClose={handleModalClose}
-          selectedItem={selectedItem}
-          selectedDrsDate={selectedDrsDate}
-          onDataFromChild={handleDataFromChild}
-        />
         <div className="page-header ">
           <div>
             <h1 className="page-title">Competitor Fuel Price</h1>
@@ -535,71 +389,58 @@ const CompetitorFuelPrices = (props) => {
             </Card>
           </Col>
         </Row>
-        <Row className="row-sm">
-          <Col lg={12}>
+        <Row>
+          <Col md={12} xl={12}>
             <Card>
-              <Card.Header>
-                <h3 className="card-title">Competitor Fuel Price</h3>
-              </Card.Header>
               <Card.Body>
-                {data ? (
-                  <div className="table-container table-responsive">
-                    <table className="table">
-                      <colgroup>
-                        {data?.head_array.map((_, index) => (
-                          <col key={index} />
-                        ))}
-                      </colgroup>
-                      <thead>
-                        <tr className="fuelprice-tr">{renderTableHeader()}</tr>
-                      </thead>
-                      <tbody>{renderTableData()}</tbody>
-                    </table>
-                  </div>
-                ) : (
-                  ""
-                )}
+                <div>
+                  {data &&
+                    data.listing.map((site) => (
+                      <div key={site.id}>
+                        <Collapse accordion>
+                          <Panel header={site.site_name} key={site.id}>
+                            <Table
+                              dataSource={[extractFuelData(site)]}
+                              columns={[
+                                {
+                                  title: "Time",
+                                  dataIndex: "time",
+                                  key: "time",
+                                },
+                                {
+                                  title: "Unleaded",
+                                  dataIndex: "unleaded",
+                                  key: "unleaded",
+                                },
+                                {
+                                  title: "Super Unleaded",
+                                  dataIndex: "superUnleaded",
+                                  key: "superUnleaded",
+                                },
+                                {
+                                  title: "Diesel",
+                                  dataIndex: "diesel",
+                                  key: "diesel",
+                                },
+                                {
+                                  title: "Super Diesel",
+                                  dataIndex: "superDiesel",
+                                  key: "superDiesel",
+                                },
+                                {
+                                  title: "Other",
+                                  dataIndex: "other",
+                                  key: "other",
+                                },
+                              ]}
+                              pagination={false}
+                            />
+                          </Panel>
+                        </Collapse>
+                      </div>
+                    ))}
+                </div>
               </Card.Body>
-              <Card.Footer>
-                {data ? (
-                  <div className="text-end notification-class">
-                    <div className="Notification">
-                      <label
-                        htmlFor="notificationCheckbox"
-                        className="form-label Notification ml-2"
-                      >
-                        <input
-                          type="checkbox"
-                          id="notificationCheckbox"
-                          checked={isChecked}
-                          onChange={SendNotification}
-                        />
-                        Send Notification
-                      </label>
-                    </div>
-
-                    {editable ? (
-                      <button
-                        className="btn btn-primary me-2"
-                        type="submit"
-                        onClick={handleSubmit}
-                      >
-                        Submit
-                      </button>
-                    ) : (
-                      <button
-                        className="btn btn-primary me-2"
-                        type="submit"
-                        disabled
-                      >
-                        Submit
-                      </button>
-                    )}
-                  </div>
-                ) : (
-                  ""
-                )}
-              </Card.Footer>
             </Card>
           </Col>
         </Row>
