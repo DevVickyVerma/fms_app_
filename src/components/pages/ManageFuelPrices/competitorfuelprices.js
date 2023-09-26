c60dbd6dcd1981e7910234484862780e11a51c92
DevVickyVerma <vicky.credentia@gmail.com>
2023-09-26 12:10:32 +0530

    Compititor Fuel Price

--- src/components/pages/ManageFuelPrices/competitorfuelprices.js
+++ src/components/pages/ManageFuelPrices/competitorfuelprices.js
@@ -250,74 +250,75 @@ const CompetitorFuelPrices = (props) => {
                         <Row>
                           {localStorage.getItem("superiorRole") !==
                             "Client" && (
-                              <Col lg={3} md={6}>
-                                <FormGroup>
-                                  <label
-                                    htmlFor="client_id"
-                                    className=" form-label mt-4"
-                                  >
-                                    Client
-                                    <span className="text-danger">*</span>
-                                  </label>
-                                  <Field
-                                    as="select"
-                                    className={`input101 ${errors.client_id && touched.client_id
+                            <Col lg={3} md={6}>
+                              <FormGroup>
+                                <label
+                                  htmlFor="client_id"
+                                  className=" form-label mt-4"
+                                >
+                                  Client
+                                  <span className="text-danger">*</span>
+                                </label>
+                                <Field
+                                  as="select"
+                                  className={`input101 ${
+                                    errors.client_id && touched.client_id
                                       ? "is-invalid"
                                       : ""
-                                      }`}
-                                    id="client_id"
-                                    name="client_id"
-                                    onChange={(e) => {
-                                      const selectedType = e.target.value;
-                                      setFieldValue("client_id", selectedType);
-                                      setSelectedClientId(selectedType);
+                                  }`}
+                                  id="client_id"
+                                  name="client_id"
+                                  onChange={(e) => {
+                                    const selectedType = e.target.value;
+                                    setFieldValue("client_id", selectedType);
+                                    setSelectedClientId(selectedType);
 
-                                      // Reset the selected company and site
-                                      setSelectedCompanyList([]);
-                                      setFieldValue("company_id", "");
-                                      setFieldValue("site_id", "");
+                                    // Reset the selected company and site
+                                    setSelectedCompanyList([]);
+                                    setFieldValue("company_id", "");
+                                    setFieldValue("site_id", "");
 
-                                      const selectedClient =
-                                        AddSiteData.data.find(
-                                          (client) => client.id === selectedType
-                                        );
+                                    const selectedClient =
+                                      AddSiteData.data.find(
+                                        (client) => client.id === selectedType
+                                      );
 
-                                      if (selectedClient) {
-                                        setSelectedCompanyList(
-                                          selectedClient.companies
-                                        );
-                                        // console.log(
-                                        //   selectedClient,
-                                        //   "selectedClient"
-                                        // );
-                                        // console.log(
-                                        //   selectedClient.companies,
-                                        //   "selectedClient"
-                                        // );
-                                      }
-                                    }}
-                                  >
-                                    <option value="">Select a Client</option>
-                                    {AddSiteData.data &&
-                                      AddSiteData.data.length > 0 ? (
-                                      AddSiteData.data.map((item) => (
-                                        <option key={item.id} value={item.id}>
-                                          {item.client_name}
-                                        </option>
-                                      ))
-                                    ) : (
-                                      <option disabled>No Client</option>
-                                    )}
-                                  </Field>
+                                    if (selectedClient) {
+                                      setSelectedCompanyList(
+                                        selectedClient.companies
+                                      );
+                                      // console.log(
+                                      //   selectedClient,
+                                      //   "selectedClient"
+                                      // );
+                                      // console.log(
+                                      //   selectedClient.companies,
+                                      //   "selectedClient"
+                                      // );
+                                    }
+                                  }}
+                                >
+                                  <option value="">Select a Client</option>
+                                  {AddSiteData.data &&
+                                  AddSiteData.data.length > 0 ? (
+                                    AddSiteData.data.map((item) => (
+                                      <option key={item.id} value={item.id}>
+                                        {item.client_name}
+                                      </option>
+                                    ))
+                                  ) : (
+                                    <option disabled>No Client</option>
+                                  )}
+                                </Field>
 
-                                  <ErrorMessage
-                                    component="div"
-                                    className="invalid-feedback"
-                                    name="client_id"
-                                  />
-                                </FormGroup>
-                              </Col>
-                            )}
+                                <ErrorMessage
+                                  component="div"
+                                  className="invalid-feedback"
+                                  name="client_id"
+                                />
+                              </FormGroup>
+                            </Col>
+                          )}
                           <Col lg={3} md={6}>
                             <FormGroup>
                               <label
@@ -329,10 +330,11 @@ const CompetitorFuelPrices = (props) => {
                               </label>
                               <Field
                                 as="select"
-                                className={`input101 ${errors.company_id && touched.company_id
-                                  ? "is-invalid"
-                                  : ""
-                                  }`}
+                                className={`input101 ${
+                                  errors.company_id && touched.company_id
+                                    ? "is-invalid"
+                                    : ""
+                                }`}
                                 id="company_id"
                                 name="company_id"
                                 onChange={(e) => {
@@ -394,10 +396,11 @@ const CompetitorFuelPrices = (props) => {
                                 type="date"
                                 min={"2023-01-01"}
                                 onClick={hadndleShowDate}
-                                className={`input101 ${errors.start_date && touched.start_date
-                                  ? "is-invalid"
-                                  : ""
-                                  }`}
+                                className={`input101 ${
+                                  errors.start_date && touched.start_date
+                                    ? "is-invalid"
+                                    : ""
+                                }`}
                                 id="start_date"
                                 name="start_date"
                               ></Field>
@@ -436,8 +439,8 @@ const CompetitorFuelPrices = (props) => {
                 <h3 className="card-title"> Competitor Fuel Price</h3>
               </Card.Header>
               <Card.Body>
-
-                {data?.length > 0 ? <>
+                {console.log(data, "Competitordata")}
+                {data ? (
                   <div>
                     {data &&
                       data.listing.map((site) => (
@@ -523,12 +526,15 @@ const CompetitorFuelPrices = (props) => {
                         </div>
                       ))}
                   </div>
-                </> : <>
-
-                  <img src={require("../../../assets/images/noDataFoundImage/noDataFound.jpg")} alt="MyChartImage" className="all-center-flex nodata-image" />
-
-                </>}
-
+                ) : (
+                  <>
+                    <img
+                      src={require("../../../assets/images/noDataFoundImage/noDataFound.jpg")}
+                      alt="MyChartImage"
+                      className="all-center-flex nodata-image"
+                    />
+                  </>
+                )}
               </Card.Body>
             </Card>
           </Col>
