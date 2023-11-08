1412ea13e140045a7c9a1d1dcb516b157319348c
jitendraCredentia <jitendra.credentia@gmail.com>
2023-11-08 11:03:33 +0530

    activity log status updated

--- src/components/pages/Emaillogs/ActivityLogs.js
+++ src/components/pages/Emaillogs/ActivityLogs.js
@@ -159,9 +159,9 @@ const ManageEmail = (props) => {
         <span className="text-muted fs-15 fw-semibold text-center">
           <OverlayTrigger placement="top" overlay={<Tooltip>Status</Tooltip>}>
             {row?.status == 1 ? (
-              <button className="btn btn-success btn-sm">Sent</button>
+              <button className="btn btn-success btn-sm">Success</button>
             ) : row?.status == 0 ? (
-              <button className="btn btn-danger btn-sm">Failed</button>
+              <button className="btn btn-danger btn-sm">Error</button>
             ) : (
               <button className="badge">Unknown</button>
             )}
