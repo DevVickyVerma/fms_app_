02772f88f83ff7b45022acf4ff88bfebbd5cd632
jitendraCredentia <jitendra.credentia@gmail.com>
2024-08-09 11:38:41 +0530

    blank page toaster added

--- src/components/Dashboard/BlankDashboard.js
+++ src/components/Dashboard/BlankDashboard.js
@@ -1,16 +1,30 @@
+import { useSelector } from 'react-redux';
 import withApi from '../../Utils/ApiHelper';
+import LoaderImg from '../../Utils/Loader';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const BlankDashboard = () => {
+    const reduxLoading = useSelector((state) => state?.data?.loading);
+
+
     return (
-        <div className='w-100 d-flex justify-content-center align-items-center' style={{ height: "100vh" }}>
-            <img
-                src={require("../../assets/images/brand/logo.png")}
-                alt="Logo"
-                className='blank-icon-image'
-                id="icon"
-                style={{ maxWidth: "180px" }}
-            />
-        </div>
+        <>
+            {reduxLoading ? <LoaderImg /> : null}
+
+            <div className='w-100 d-flex justify-content-center align-items-center' style={{ height: "100vh" }}>
+                <img
+                    src={require("../../assets/images/brand/logo.png")}
+                    alt="Logo"
+                    className='blank-icon-image'
+                    id="icon"
+                    style={{ maxWidth: "180px" }}
+                />
+            </div>
+
+            <ToastContainer />
+        </>
+
     );
 };
 
