1f0e9c250dc0674b135ecdc082ec042bca26ffa7
vicky654 <vickyverma2026@gmail.com>
2024-09-23 11:37:25 +0530

    fix Time issues

--- src/components/pages/Competitor/InputTime.js
+++ src/components/pages/Competitor/InputTime.js
@@ -41,6 +41,7 @@ const InputTime = ({ label, value, onChange, className, disabled }) => {
           onKeyDown={(e) => e.preventDefault()}
           onKeyUp={(e) => e.preventDefault()}
           minutesStep={1}
+          desktopModeMediaQuery="(min-width: 0px)"
           disabled={disabled} // Disable the TimePicker when not editable
           renderInput={(params) => (
             <TextField {...params} fullWidth label={label} disabled={disabled} />
@@ -53,55 +54,3 @@ const InputTime = ({ label, value, onChange, className, disabled }) => {
 
 export default InputTime;
 
-
-// import React, { useEffect, useState } from 'react';
-// import { LocalizationProvider, TimePicker } from '@mui/x-date-pickers';
-// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-// import TextField from '@mui/material/TextField';
-// import dayjs from 'dayjs';
-
-// const InputTime = ({ label, value, onChange, className, disabled }) => {
-//   const [timeValue, setTimeValue] = useState(null);
-
-//   useEffect(() => {
-//     // Ensure value is in 'HH:mm' format and update timeValue accordingly
-//     if (value) {
-//       const parsedValue = dayjs(value, 'HH:mm');
-//       if (parsedValue.isValid()) {
-//         setTimeValue(parsedValue); // Set dayjs object
-//       }
-//     }
-//   }, [value]); // Run when value changes
-
-//   return (
-//     <span
-//       className='input-time-small-screen'
-//       onKeyDown={(e) => e.preventDefault()}
-//       onKeyUp={(e) => e.preventDefault()}
-//     >
-//       <LocalizationProvider dateAdapter={AdapterDayjs} className="input-time-picker">
-//         <TimePicker
-//           value={timeValue}
-//           onChange={(newValue) => {
-//             if (!disabled) { // Only update if not disabled
-//               const formattedTime = newValue ? newValue.format('HH:mm') : '';
-//               setTimeValue(newValue); // Update timeValue state
-//               onChange(formattedTime); // Call parent onChange with formatted time
-//             }
-//           }}
-//           className={` ${className}`}
-//           onKeyDown={(e) => e.preventDefault()}
-//           onKeyUp={(e) => e.preventDefault()}
-//           minutesStep={1}
-//           disabled={disabled} // Disable the TimePicker when not editable
-//           ampm={false} // Enforce 24-hour format
-//           renderInput={(params) => (
-//             <TextField {...params} fullWidth label={label} disabled={disabled} />
-//           )}
-//         />
-//       </LocalizationProvider>
-//     </span>
-//   );
-// };
-
-// export default InputTime;
