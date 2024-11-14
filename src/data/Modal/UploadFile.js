3f58db5270b79ca972fa11640164ec91caa2f2c1
jitendraCredentia <jitendra.credentia@gmail.com>
2024-11-14 18:01:56 +0530

    modal updated

--- src/data/Modal/UploadFile.js
+++ src/data/Modal/UploadFile.js
@@ -1,20 +1,14 @@
-import React, { useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from "@mui/material";
-import { Button } from "react-bootstrap";
+import { useState } from "react";
+import { Card, Modal } from "react-bootstrap";
 import { Formik, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
-
 import Loaderimg from "../../Utils/Loader";
-
 import { Slide, toast } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export function FormModal(props) {
-  const { showModal, setShowModal } = props;
+  const { showModal, setShowModal, modalTitle } = props;
   const [isLoading, setIsLoading] = useState(false);
 
   const handleClose = () => {
@@ -130,77 +124,95 @@ export function FormModal(props) {
     <>
       {isLoading ? <Loaderimg /> : null}
       <div>
-        <Dialog open={showModal} onClose={handleClose}>
-          <DialogTitle>
-            {props.modalTitle}
-            <Button onClick={handleClose} className="btn-close" variant="">
-              x
-            </Button>
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText>{props.modalContentText}</DialogContentText>
 
-            <Formik
-              initialValues={{
-                image: null,
+        <Modal show={showModal} onHide={handleClose} centered className='dashboard-center-modal' >
+
+          <div >
+            <Modal.Header
+              style={{
+                color: "#fff",
               }}
-              validationSchema={Yup.object().shape({
-                image: Yup.mixed()
-                  .required("File is required")
-                  .test("fileType", "Invalid file type", (value) => {
-                    if (value) {
-                      const allowedFileTypes = [
-                        "text/csv",
-                        "application/vnd.ms-excel",
-                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                      ];
-                      return allowedFileTypes.includes(value.type);
-                    }
-                    return true;
-                  }),
-              })}
-              onSubmit={(values) => {
-                handleSubmit1(values);
-              }}
+              className='p-0 m-0 d-flex justify-content-between align-items-center'
             >
-              {({ handleSubmit, errors, touched, setFieldValue }) => (
-                <Form onSubmit={handleSubmit}>
-                  <div className="form-group">
-                    <label htmlFor="image">Image</label>
-                    <div
-                      className={`dropzone ${errors.image && touched.image ? "is-invalid" : ""
-                        }`}
-                      onDrop={(event) => handleDrop(event, setFieldValue)}
-                      onDragOver={(event) => event.preventDefault()}
-                    >
-                      <input
-                        type="file"
-                        id="image"
-                        name="image"
-                        onChange={(event) =>
-                          handleImageChange(event, setFieldValue)
-                        }
-                        className="form-control"
-                      />
-                      <p>Upload image here, or click to browse</p>
-                    </div>
-                    <ErrorMessage
-                      component="div"
-                      className="invalid-feedback"
-                      name="image"
-                    />
-                  </div>
 
-                  <div className="text-end">
-                    <button className="btn btn-primary me-2 " type="submit">
-                      Upload
-                    </button>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-          </DialogContent>
-        </Dialog>
+              <span className="ModalTitle d-flex justify-content-between w-100  fw-normal"  >
+                <span>
+                  {props.modalContentText}
+                  {modalTitle}
+                </span>
+                <span onClick={handleClose} >
+                  <button className="close-button">
+                    <FontAwesomeIcon icon={faTimes} />
+                  </button>
+                </span>
+              </span>
+            </Modal.Header>
+            <Card.Body>
+              <Formik
+                initialValues={{
+                  image: null,
+                }}
+                validationSchema={Yup.object().shape({
+                  image: Yup.mixed()
+                    .required("File is required")
+                    .test("fileType", "Invalid file type", (value) => {
+                      if (value) {
+                        const allowedFileTypes = [
+                          "text/csv",
+                          "application/vnd.ms-excel",
+                          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        ];
+                        return allowedFileTypes.includes(value.type);
+                      }
+                      return true;
+                    }),
+                })}
+                onSubmit={(values) => {
+                  handleSubmit1(values);
+                }}
+              >
+                {({ handleSubmit, errors, touched, setFieldValue }) => (
+                  <Form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="image">File</label>
+                      <div
+                        className={`dropzone ${errors.image && touched.image ? "is-invalid" : ""
+                          }`}
+                        onDrop={(event) => handleDrop(event, setFieldValue)}
+                        onDragOver={(event) => event.preventDefault()}
+                      >
+                        <input
+                          type="file"
+                          id="image"
+                          name="image"
+                          onChange={(event) =>
+                            handleImageChange(event, setFieldValue)
+                          }
+                          className="form-control"
+                        />
+                        <p className="small mt-1">Upload File here, or click to browse</p>
+                      </div>
+                      <ErrorMessage
+                        component="div"
+                        className="invalid-feedback"
+                        name="image"
+                      />
+                    </div>
+
+                    <div className="text-end">
+                      <button className="btn btn-primary me-2 " type="submit">
+                        Upload
+                      </button>
+                    </div>
+                  </Form>
+                )}
+              </Formik>
+            </Card.Body>
+
+          </div>
+        </Modal>
+
+
       </div>
     </>
   );
