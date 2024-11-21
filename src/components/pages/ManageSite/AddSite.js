d84829c23d91a5e81fc23b0bedaa0a3bcb3959c6
vicky654 <vickyverma2026@gmail.com>
2024-11-21 21:12:28 +0530

    fix issues

--- src/components/pages/ManageSite/AddSite.js
+++ src/components/pages/ManageSite/AddSite.js
@@ -6,7 +6,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import withApi from "../../../Utils/ApiHelper";
 import Loaderimg from "../../../Utils/Loader";
 import useErrorHandler from "../../CommonComponent/useErrorHandler";
-import { AddSiteinitialValues, AddSitevalidationSchema } from '../../../Utils/commonFunctions/formConfig';
+import { AddSiteinitialValues } from '../../../Utils/commonFunctions/formConfig';
 import FormikSelect from '../../Formik/FormikSelect';
 import FormikInput from '../../Formik/FormikInput';
 import { ReactMultiEmail } from "react-multi-email";
@@ -28,11 +28,64 @@ const AddSite = (props) => {
       handleError(error);
     }
   };
-
+  const AddSiteinitialValues = {
+    site_code: "",
+    site_name: "",
+    clients: [],
+    site_Address: "",
+    Site_Status: "",
+    bussiness_Type: "",
+    Select_machine_type: "",
+    supplier: "",
+    DRS_Start_Date: "",
+    display_name: "",
+    Saga_department_code: "",
+    Saga_department_name: "",
+    Bp_nctt_site_no: "",
+    bank_ref: "",
+    Report_generation_Status: "",
+    Report_date_type: "",
+    consider_keyfules_cards: "",
+    Fuel_commission_type: "",
+    Paper_work_status: "",
+    Bunkered_sale_status: "",
+    Drs_upload_status: "",
+    client_id: "",
+    company_id: "",
+    lottery_commission: "",
+    instant_lottery_commission: "",
+    e_code: "",
+    paypoint_commission: "",
+    cashback_enable: 0,
+    shop_commission: 0,
+    paidout: 0,
+    apply_sc: 0,
+    is_reconciled: 0,
+    auto_dayend: 0,
+    ignore_tolerance: 0,
+    d_deduction: 1,
+    display_all_sales: 1,
+    security_amount: "",
+    loomis_status: 0,
+    cashback_status: 0,
+    fuel_discount: 0,
+    vat_summary: 0,
+    include_bunkered_sales: 0,
+    show_admin_sale: 0,
+    send_auto_report: 0,
+    consider_fuel_sales: 1,
+    shop_sale_file_upload: 1,
+    update_tlm_price: 0,
+    change_back_date_price: 0,
+    to_emails: [],
+    cc_emails: [],
+    mobile: "",
+  };
   const formik = useFormik({
     initialValues: AddSiteinitialValues,
     validationSchema: Yup.object({
       site_code: Yup.string().required("Site Code is required"),
+      e_code: Yup.string().required("Site ID is required"),
       site_name: Yup.string()
         .max(150, "Must be 150 characters or less")
         .required("Site Name is required"),
@@ -60,17 +113,102 @@ const AddSite = (props) => {
       d_deduction: Yup.string().required("Deduct Deduction is required"),
       display_all_sales: Yup.string().required("Display All Sales is required"),
       cashback_enable: Yup.string().required("Cashback Enable is required"),
-      update_tlm_price: Yup.string().required("Update TLM Price is required"),
-      to_emails: Yup.array()
-        .of(Yup.string().email("Invalid email format"))
-        .min(1, "At least one email is required"),
+      // update_tlm_price: Yup.string().required("Update TLM Price is required"),
+      // to_emails: Yup.array()
+      //   .of(Yup.string().email("Invalid email format"))
+      //   .min(1, "At least one email is required"),
     }),
     onSubmit: (values) => {
+      handleSubmit1(values)
       console.log("Form data", values);
     },
   });
+ 
+  const handleSubmit1 = async (values,) => {
+    try {
+      const formData = new FormData();
+      formData.append("business_type_id", values.bussiness_Type);
+      formData.append("data_import_type_id", values.Select_machine_type);
+      formData.append("site_code", values.site_code);
+      formData.append("site_name", values.site_name);
+      formData.append("site_display_name", values.display_name);
+      formData.append("site_address", values.site_Address);
+      formData.append("start_date", values.DRS_Start_Date);
+      formData.append("department_sage_code", values.Saga_department_name);
+      formData.append("bp_credit_card_site_no", values.Bp_nctt_site_no);
+      formData.append("bank_ref", values.bank_ref);
+      formData.append("supplier_id", values.supplier);
+      formData.append("site_report_status", values.Report_generation_Status);
+      formData.append("site_report_date_type", values.Report_date_type);
+      formData.append(
+        "consider_keyfules_cards",
+        values.consider_keyfules_cards
+      );
+      formData.append("sage_department_id", values.Saga_department_code);
+      formData.append("drs_upload_status", values.Drs_upload_status);
+      formData.append("site_status", values.Site_Status);
+      formData.append("security_amount", values.security_amount);
+      formData.append("bunker_upload_status", values.Bunkered_sale_status);
+      formData.append(
+        "fuel_commission_calc_status",
+        values.Fuel_commission_type
+      );
+      formData.append("paperwork_status", values.Paper_work_status);
+      formData.append("company_id", values.company_id);
+      formData.append("client_id", values.client_id);
+      formData.append("lottery_commission", 0);
+      formData.append("paypoint_commission", values.paypoint_commission);
+      formData.append(
+        "instant_lottery_commission",
+        values.instant_lottery_commission
+      );
+      formData.append("shop_commission", 0);
+      formData.append("paidout", values.paidout);
+      formData.append("apply_sc", values.apply_sc);
+      formData.append("loomis_status", values.loomis_status);
+      formData.append("cashback_status", values.cashback_status);
+      formData.append("auto_dayend", values.auto_dayend);
+      formData.append("ignore_tolerance", values.ignore_tolerance);
+      formData.append("d_deduction", values.d_deduction);
+      formData.append("display_all_sales", values.display_all_sales);
+      formData.append("is_reconciled", values.is_reconciled);
+      formData.append("fuel_discount", values.fuel_discount);
+      formData.append("vat_summary", values.vat_summary);
+      formData.append("include_bunkered_sales", values.include_bunkered_sales);
+      formData.append("show_admin_sale", values.show_admin_sale);
+      formData.append("send_auto_report", values.send_auto_report);
+      formData.append("consider_fuel_sales", values.consider_fuel_sales);
+      formData.append("shop_sale_file_upload", values.shop_sale_file_upload);
+      formData.append("update_tlm_price", values.update_tlm_price);
+      formData.append("change_back_date_price", values.change_back_date_price);
+      formData.append("cashback_enable", values.cashback_enable);
+      formData.append("e_code", values.e_code);
 
 
+      if (values?.to_emails && values?.to_emails.length > 0) {
+        values?.to_emails.forEach((client, index) => {
+          formData.append(`to_emails[${index}]`, client);
+        });
+      } else {
+        formData.append(`to_emails`, []);
+      }
+
+      if (values?.cc_emails && values?.cc_emails.length > 0) {
+        values?.cc_emails.forEach((client, index) => {
+          formData.append(`cc_emails[${index}]`, client);
+        });
+      } else {
+        formData.append(`cc_emails`, []);
+      }
+
+      const postDataUrl = "/site/addss";
+
+      const navigatePath = "/sites";
+      await postData(postDataUrl, formData, navigatePath); // Set the submission state to false after the API call is completed
+    } catch (error) {
+      console.error(error); // Set the submission state to false if an error occurs
+    }
+  };
 
 
 
@@ -179,9 +317,7 @@ const AddSite = (props) => {
 
       }
 
-      if (parsedData?.company_id) {
-        GetSiteList(parsedData?.company_id);
-      }
+
     }
 
     if (!storedDataString && localStorage.getItem("superiorRole") === "Client") {
@@ -226,7 +362,7 @@ const AddSite = (props) => {
     // Update Formik state when email input changes
     formik.setFieldValue("cc_emails", newEmails);
   };
-console.log(formik.values.update_tlm_price, "formik.values.update_tlm_price");
+  console.log(formik.values.update_tlm_price, "formik.values.update_tlm_price");
   const renderCCEmailTag = (email, index, removeEmail) => (
     <div key={index} className="renderEmailTag">
       {email}
@@ -235,7 +371,7 @@ console.log(formik.values.update_tlm_price, "formik.values.update_tlm_price");
       </span>
     </div>
   );
-console.log(formik.values, "closeicon");
+  console.log(formik.values, "closeicon");
 
   return (
     <>
@@ -313,7 +449,7 @@ console.log(formik.values, "closeicon");
                           label="Supplier"
                           options={AddSiteData.suppliers?.map((item) => ({ id: item?.id, name: item?.supplier_name }))}
                           className="form-input"
-                          onChange={handleCompanyChange}
+                          // onChange={handleCompanyChange}
                         />
                       </Col>
                       <Col lg={4}>
@@ -323,7 +459,7 @@ console.log(formik.values, "closeicon");
                         <FormikInput formik={formik} type="text" name="site_name" />
                       </Col>
                       <Col lg={4}>
-                        <FormikInput formik={formik} type="text" label='Site Id' name="e_code" />
+                        <FormikInput formik={formik} type="text" label='Site Id'  name="e_code" />
                       </Col>
                       <Col lg={4}>
                         <FormikInput formik={formik} type="text" name="display_name" />
@@ -341,7 +477,7 @@ console.log(formik.values, "closeicon");
                       <Col lg={4} md={6}>
                         <FormikSelect
                           formik={formik}
-                          name="Saga_department_code"
+                          name="bussiness_Type"
                           label="Bussiness Type"
                           options={AddSiteData.busines_types?.map((item) => ({ id: item?.id, name: item?.name }))}
                           className="form-input"
@@ -646,7 +782,7 @@ console.log(formik.values, "closeicon");
                           formik={formik}
                           name="update_tlm_price"
                           label="update_tlm_price"
-                          options={yesNoOptions?.map((item) => ({ id: item?.value, name: item?.label}))}
+                          options={yesNoOptions?.map((item) => ({ id: item?.value, name: item?.label }))}
                           className="form-input"
                           onChange={handleTLMPriceChange}
 
@@ -731,25 +867,26 @@ console.log(formik.values, "closeicon");
 
 
                     </Row>
+                    <Card.Footer className="text-end">
+                      <Link
+                        type="submit"
+                        className="btn btn-danger me-2 "
+                        to={`/sites/`}
+                      >
+                        Cancel
+                      </Link>
+
+                      <button
+                        type="submit"
+                        className="btn btn-primary me-2 "
+                      // disabled={Object.keys(errors).length > 0}
+                      >
+                        Save
+                      </button>
+                    </Card.Footer>
                   </form>
                 </Card.Body>
-                <Card.Footer className="text-end">
-                  <Link
-                    type="submit"
-                    className="btn btn-danger me-2 "
-                    to={`/sites/`}
-                  >
-                    Cancel
-                  </Link>
 
-                  <button
-                    type="submit"
-                    className="btn btn-primary me-2 "
-                  // disabled={Object.keys(errors).length > 0}
-                  >
-                    Save
-                  </button>
-                </Card.Footer>
               </Card>
 
             </Col>
