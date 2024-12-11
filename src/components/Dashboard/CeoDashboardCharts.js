bf0c396ca42b865ba61c8fcca4dbcd5b4a62f840
jitendraCredentia <jitendrasaini352jjnu@gmail.com>
2024-12-11 17:47:29 +0530

    date range inital defined  selected

--- src/components/Dashboard/CeoDashboardCharts.js
+++ src/components/Dashboard/CeoDashboardCharts.js
@@ -3,35 +3,56 @@ import CeoDashboardBarChart from "./CeoDashboardBarChart";
 import SmallLoader from "../../Utils/SmallLoader";
 import NoDataComponent from "../../Utils/commonFunctions/NoDataComponent";
 
-const CeoDashboardCharts = ({ Salesstatsloading, BarGraphSalesStats, Baroptions, selectedOption }) => {
+const CeoDashboardCharts = ({
+  Salesstatsloading,
+  BarGraphSalesStats,
+  Baroptions,
+  selectedOption,
+  formik,
+}) => {
+  const renderChartOrLoader = (data, title) => {
+    if (Salesstatsloading) {
+      return <SmallLoader />;
+    }
 
-    const renderChartOrLoader = (data, title) => {
-        if (Salesstatsloading) {
-            return <SmallLoader />;
-        }
+    console.log(formik?.values, "forasdasdasdmik");
 
-        if (!data || data.length === 0) {
-            return <NoDataComponent title={title} />; // Display a message or fallback when data is empty
-        }
-
-        return <CeoDashboardBarChart data={data} options={Baroptions} title={title} selectedOption={selectedOption} />;
-    };
-
+    if (!data || data.length === 0) {
+      return <NoDataComponent title={title} />; // Display a message or fallback when data is empty
+    }
 
     return (
-        <Row className="mb-4">
-            <Col sm={12} md={6} xl={6} key="chart-1">
-                {renderChartOrLoader(BarGraphSalesStats?.sales_mom, "Current Month vs Previous Month")}
-            </Col>
-            <Col sm={12} md={6} xl={6} key="chart-2">
-                {renderChartOrLoader(BarGraphSalesStats?.sales_actual_budgeted, "Actual Sales vs Budgeted Sales")}
-            </Col>
-            <Col sm={12} md={6} xl={6} key="chart-3" className="mt-4">
-                {renderChartOrLoader(BarGraphSalesStats?.sales_yoy, "Same Month Sales vs Previous Year’s Month Sales")}
-            </Col>
-
-        </Row>
+      <CeoDashboardBarChart
+        data={data}
+        options={Baroptions}
+        title={title}
+        selectedOption={selectedOption}
+      />
     );
+  };
+
+  return (
+    <Row className="mb-4">
+      <Col sm={12} md={6} xl={6} key="chart-1">
+        {renderChartOrLoader(
+          BarGraphSalesStats?.sales_mom,
+          "Current Month vs Previous Month"
+        )}
+      </Col>
+      <Col sm={12} md={6} xl={6} key="chart-2">
+        {renderChartOrLoader(
+          BarGraphSalesStats?.sales_actual_budgeted,
+          "Actual Sales vs Budgeted Sales"
+        )}
+      </Col>
+      <Col sm={12} md={6} xl={6} key="chart-3" className="mt-4">
+        {renderChartOrLoader(
+          BarGraphSalesStats?.sales_yoy,
+          "Same Month Sales vs Previous Year’s Month Sales"
+        )}
+      </Col>
+    </Row>
+  );
 };
 
 export default CeoDashboardCharts;
