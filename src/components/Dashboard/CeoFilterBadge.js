ee16e420a9cbd326f1ffead2f6f822e93ddd9986
jitendraCredentia <jitendrasaini352jjnu@gmail.com>
2024-12-12 18:02:15 +0530

    Ui design created

--- src/components/Dashboard/CeoFilterBadge.js
+++ src/components/Dashboard/CeoFilterBadge.js
@@ -11,29 +11,31 @@ const CeoFilterBadge = ({
     }
   };
   return (
-
-
-    <div className="d-flex gap-2 flex-wrap btn" >
-      {filters?.client_name || filters?.company_name || filters?.site_name || filters?.start_date ? (
+    <div className="d-flex gap-2 flex-wrap btn">
+      {filters?.client_name ||
+      filters?.company_name ||
+      filters?.site_name ||
+      filters?.start_date ? (
         <div
-          className="badges-container d-flex flex-wrap align-items-center gap-2 px-4 py-sm-0 py-2 text-white"
+          className="badges-container d-flex flex-wrap align-items-center gap-2 px-4 py-sm-2 py-2 text-white"
           style={{ background: "#ddd" }}
         >
           {filters?.client_name && (
             <div className="badge bg-blue-600 d-flex align-items-center gap-2 p-3 position-relative">
-              <span className="font-semibold">Client :</span> {filters?.client_name}
-
+              <span className="font-semibold">Client :</span>{" "}
+              {filters?.client_name}
             </div>
           )}
           {filters?.company_name && (
             <div className="badge bg-green-600 d-flex align-items-center gap-2 p-3 position-relative">
-              <span className="font-semibold">Company :</span> {filters?.company_name}
+              <span className="font-semibold">Company :</span>{" "}
+              {filters?.company_name}
               <button
-                className="btn btn-danger btn-sm position-absolute"
-                style={{ top: "-20px", right: "-20px", borderRadius: "50%" }}
+                className="btn btn-danger btn-sm position-absolute ceo-cross-icon"
+                // style={{ top: "-20px", right: "-20px", borderRadius: "50%" }}
                 onClick={() => handleRemoveFilter("company_name")}
               >
-                &times;
+                <span>&times;</span>
               </button>
             </div>
           )}
@@ -41,23 +43,22 @@ const CeoFilterBadge = ({
             <div className="badge bg-red-600 d-flex align-items-center gap-2 p-3 position-relative">
               <span className="font-semibold">Site :</span> {filters?.site_name}
               <button
-                className="btn btn-danger btn-sm position-absolute"
-                style={{ top: "-20px", right: "-20px", borderRadius: "50%" }}
+                className="btn btn-danger btn-sm position-absolute ceo-cross-icon"
                 onClick={() => handleRemoveFilter("site_name")}
               >
-                &times;
+                <span>&times;</span>
               </button>
             </div>
           )}
           {filters?.start_date && showStartDate && (
             <div className="badge bg-yellow-600 d-flex align-items-center gap-2 p-3 position-relative">
-              <span className="font-semibold">Start Date :</span> {filters?.start_date}
+              <span className="font-semibold">Start Date :</span>{" "}
+              {filters?.start_date}
               <button
-                className="btn btn-danger btn-sm position-absolute"
-                style={{ top: "-20px", right: "-20px", borderRadius: "50%" }}
+                className="btn btn-danger btn-sm position-absolute ceo-cross-icon"
                 onClick={() => handleRemoveFilter("start_date")}
               >
-                &times;
+                <span>&times;</span>
               </button>
             </div>
           )}
@@ -69,15 +70,16 @@ const CeoFilterBadge = ({
           </span>
         </div>
       )}
-      {
-        (filters?.client_name || filters?.company_name || filters?.site_name || filters?.start_date) &&
+      {/* {(filters?.client_name ||
+        filters?.company_name ||
+        filters?.site_name ||
+        filters?.start_date) &&
         showResetBtn && (
           <span onClick={handleResetFilters} className="btn btn-danger">
             <i className="ph ph-arrow-clockwise" />
           </span>
-        )
-      }
-    </div >
+        )} */}
+    </div>
   );
 };
 
