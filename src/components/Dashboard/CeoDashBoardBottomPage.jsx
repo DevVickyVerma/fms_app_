09f439bdad8ff475503cbfc134266ef43206038d
jitendraCredentia <jitendrasaini352jjnu@gmail.com>
2024-12-16 11:31:29 +0530

    stock deatil icon updated

--- src/components/Dashboard/CeoDashBoardBottomPage.jsx
+++ src/components/Dashboard/CeoDashBoardBottomPage.jsx
@@ -10,6 +10,7 @@ import {
 } from "react-icons/fa";
 import CeoDetailModal from "../../components/Dashboard/CeoDashboardModal/CeoDetailModal";
 import { useSelector } from "react-redux";
+import { GiVendingMachine } from "react-icons/gi";
 
 const CeoDashBoardBottomPage = (props) => {
   const { getData, filters, applyNavigate } = props;
@@ -90,8 +91,9 @@ const CeoDashBoardBottomPage = (props) => {
                     alignItems: "center",
                     justifyContent: "center",
                   }}
-                  className={` ${applyNavigate ? "pointer ceo-sats-card-hover" : ""
-                    }`}
+                  className={` ${
+                    applyNavigate ? "pointer ceo-sats-card-hover" : ""
+                  }`}
                 >
                   <FaChartLine size={25} />
                   <h3 style={{ fontSize: "18px" }} className="m-0 ms-2">
@@ -114,8 +116,9 @@ const CeoDashBoardBottomPage = (props) => {
                   alignItems: "center",
                   justifyContent: "center",
                 }}
-                className={` ${applyNavigate ? "pointer ceo-sats-card-hover" : ""
-                  }`}
+                className={` ${
+                  applyNavigate ? "pointer ceo-sats-card-hover" : ""
+                }`}
               >
                 <h3 style={{ fontSize: "18px" }} className="m-0">
                   <img
@@ -145,8 +148,9 @@ const CeoDashBoardBottomPage = (props) => {
                       className="slide-in-left "
                     >
                       <Card
-                        className={` ${applyNavigate ? "pointer ceocard-hover" : ""
-                          }`}
+                        className={` ${
+                          applyNavigate ? "pointer ceocard-hover" : ""
+                        }`}
                         style={{
                           backgroundColor: "#4663ac", // Red background
                           color: "#fff",
@@ -174,8 +178,9 @@ const CeoDashBoardBottomPage = (props) => {
                       className="slide-in-left "
                     >
                       <Card
-                        className={` ${applyNavigate ? "pointer ceocard-hover" : ""
-                          }`}
+                        className={` ${
+                          applyNavigate ? "pointer ceocard-hover" : ""
+                        }`}
                         style={{
                           backgroundColor: "#6764b3", // Dark Blue background
                           color: "#fff",
@@ -203,8 +208,9 @@ const CeoDashBoardBottomPage = (props) => {
                       className="slide-in-right"
                     >
                       <Card
-                        className={` ${applyNavigate ? "pointer ceocard-hover" : ""
-                          }`}
+                        className={` ${
+                          applyNavigate ? "pointer ceocard-hover" : ""
+                        }`}
                         style={{
                           backgroundColor: "#8563b6", // Light Blue background
                           color: "#fff",
@@ -232,8 +238,9 @@ const CeoDashBoardBottomPage = (props) => {
                       className="slide-in-right"
                     >
                       <Card
-                        className={` ${applyNavigate ? "pointer ceocard-hover" : ""
-                          }`}
+                        className={` ${
+                          applyNavigate ? "pointer ceocard-hover" : ""
+                        }`}
                         style={{
                           backgroundColor: "#7e95e4", // Yellow background
                           color: "#fff",
@@ -265,8 +272,9 @@ const CeoDashBoardBottomPage = (props) => {
               className="slide-in-bottom"
             >
               <Card
-                className={`card-default-height ${applyNavigate ? "pointer ceocard-hover" : ""
-                  }`}
+                className={`card-default-height ${
+                  applyNavigate ? "pointer ceocard-hover" : ""
+                }`}
                 style={{
                   backgroundColor: "#8563b6", // Light Blue background
                   color: "#fff",
@@ -293,8 +301,9 @@ const CeoDashBoardBottomPage = (props) => {
               className="slide-in-bottom"
             >
               <Card
-                className={`card-default-height ${applyNavigate ? "pointer ceocard-hover" : ""
-                  }`}
+                className={`card-default-height ${
+                  applyNavigate ? "pointer ceocard-hover" : ""
+                }`}
                 style={{
                   backgroundColor: "#7e95e4", // Dark Blue background
                   color: "#fff",
@@ -319,8 +328,9 @@ const CeoDashBoardBottomPage = (props) => {
               className="slide-in-bottom"
             >
               <Card
-                className={`card-default-height ${applyNavigate ? "pointer ceocard-hover" : ""
-                  }`}
+                className={`card-default-height ${
+                  applyNavigate ? "pointer ceocard-hover" : ""
+                }`}
                 style={{
                   backgroundColor: "#4663ac", // Red background
                   color: "#fff",
@@ -334,7 +344,7 @@ const CeoDashBoardBottomPage = (props) => {
                   transition: "all 0.3s ease", // Smooth opacity transition
                 }}
               >
-                <FaGasPump size={40} />
+                <GiVendingMachine size={40} />
                 <h5 className="m-0 mt-2">Stock Details</h5>
               </Card>
             </Col>
