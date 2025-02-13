958f2f0c938f7c8bb0d0b576aed1ce7bd3309ec6
DevVickyVerma <vicky.credentia@gmail.com>
2025-02-13 15:18:55 +0530

    config Update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,22 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+// import type { CapacitorConfig } from '@capacitor/cli';
+
+// const config: CapacitorConfig = {
+//   appId: 'com.example.app',
+//   appName: 'FMS-credentia',
+//   webDir: 'build'
+// };
+
+// export default config;
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'FMS-credentia',
-  webDir: 'build'
+  appId: "com.example.app",
+  appName: "FMS-credentia",
+  webDir: "build",
+  server: { url: "http://192.168.1.186:3000", cleartext: true },
+  plugins: {
+    PushNotifications: { presentationOptions: ["badge", "sound", "alert"] },
+  },
 };
 
 export default config;
