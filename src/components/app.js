3bae8a499ccd91021b1c927bbebccd05715c93ca
vicky654 <vickyverma2026@gmail.com>
2025-03-03 16:11:22 +0530

    Update code

--- src/components/app.js
+++ src/components/app.js
@@ -257,13 +257,7 @@ const App = () => {
                     <Header />
                     <Sidebar />
 
-                    <div
-<<<<<<< HEAD
-                      className={`main-content app-content ${isMobile ? "app-mobile-main-content" : ""}`}
-=======
-                      className={`main-content app-content ${!deviceInfo?.isVirtual ? "app-mobile-main-content" : ""}`}
->>>>>>> d1ad9c67fe837721c15a4b0c128b6abd9db0d418
-                    >
+                    <div className={`main-content app-content ${isMobile ? "app-mobile-main-content" : ""}`}>
                       <div className="side-app">
                         <div className="main-container container-fluid">
                           <Outlet />
