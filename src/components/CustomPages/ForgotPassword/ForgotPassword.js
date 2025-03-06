78496f6f01a283ac30f1296509426987ad8b4a69
vicky654 <vickyverma2026@gmail.com>
2025-03-06 15:00:21 +0530

    fix forgot sacreen password

--- src/components/CustomPages/ForgotPassword/ForgotPassword.js
+++ src/components/CustomPages/ForgotPassword/ForgotPassword.js
@@ -42,148 +42,119 @@ export default function ForgotPassword() {
   };
 
   return (
-    <div className="login-img overflow-hidden">
+    <>
       {isLoading ? <Loaderimg /> : null}
-
-      <Row>
-        {/* <Col lg={7} sm={0} className="c-login-left-card">
-          <div className="page">
-
-            <div className=" d-flex align-items-center justify-content-center">
-              <img
-                src={require("../../../assets/images/login.png")}
-                alt="MyChartImage"
-                className="c-login-img"
-              />
-            </div>
-          </div>
-        </Col> */}
-
-        <Col lg={12} sm={12} className="c-login-left-card">
-          <div className="page">
-            <div className="container-login100 p-0 d-flex justify-content-center">
-              <div className="wrap-login100 p-0">
-                <div className="col col-login mx-auto">
-                  <div className="text-center login-logo"></div>
-                </div>
-                <div className="container-login100 p-0">
-                  <Row>
-                    <Col className=" col-login  mx-auto">
-                      <Formik
-                        initialValues={{
-                          email: "",
-                        }}
-                        validationSchema={ForgotPasswordSchema}
-                        onSubmit={(values) => {
-                          handleSubmit(values);
-                        }}
-                      >
-                        {() => (
-                          <Form className=" shadow-none p-0 m-0" method="post">
-                            <Card.Body className="mx-auto">
-                              <div
-                                display={"flex"}
-                                flexDirection={"column"}
-                                justifyContent={"center"}
-                                alignItems={"center"}
-                                width={"271px"}
-                                className="forgot-body"
-                              >
-                                <div className="text-center ">
-                                  <span
-                                    className="login100-form-title"
-                                    style={{ paddingBottom: "12px" }}
-                                  >
-                                    <img
-                                      src={require("../../../assets/images/brand/logo.png")}
-                                      className="header-brand-img"
-                                      alt=""
-                                    />
-                                  </span>
-                                  <p className=" m-0 p-0 font-weight-bold">
-                                    Forgot Password
-                                  </p>
-                                  {/* <p className="text-muted">
+      <div className="login-img overflow-hidden">
+        <Row>
+          <Col lg={12} md={12} sm={12} className="c-login-left-card">
+            <div className="page" >
+              <div className="container-login100 d-flex justify-content-center">
+                <div className="wrap-login100 p-0">
+                  <Card.Body>
+                    <Formik
+                      initialValues={{
+                        email: "",
+                      }}
+                      validationSchema={ForgotPasswordSchema}
+                      onSubmit={(values) => {
+                        handleSubmit(values);
+                      }}
+                    >
+                      {() => (
+                        <Form className=" shadow-none p-0 m-0" method="post">
+                          <Card.Body className="mx-auto">
+                            <div
+                              display={"flex"}
+                              flexDirection={"column"}
+                              justifyContent={"center"}
+                              alignItems={"center"}
+                              width={"271px"}
+                              className="forgot-body"
+                            >
+                              <div className="text-center ">
+                                <span
+                                  className="login100-form-title"
+                                  style={{ paddingBottom: "12px" }}
+                                >
+                                  <img
+                                    src={require("../../../assets/images/brand/logo.png")}
+                                    className="header-brand-img"
+                                    alt=""
+                                  />
+                                </span>
+                                <p className=" m-0 p-0 font-weight-bold">
+                                  Forgot Password
+                                </p>
+                                {/* <p className="text-muted">
                                     Enter the email address registered on your account
                                   </p> */}
+                              </div>
+                              <div className="pt-3 w-100" id="forgot">
+                                <div className="form-group">
+                                  <label
+                                    className="form-label"
+                                    htmlFor="email"
+                                  >
+                                    E-Mail
+                                  </label>
+                                  <Field
+                                    className="form-control"
+                                    name="email"
+                                    placeholder="Enter Your Email"
+                                    type="email"
+                                  />
+                                  <ErrorMessage
+                                    className="text-danger"
+                                    name="email"
+                                    component="div"
+                                  />
                                 </div>
-                                <div className="pt-3 w-100" id="forgot">
-                                  <div className="form-group">
-                                    <label
-                                      className="form-label"
-                                      htmlFor="email"
+                                <div className="container-login100-form-btn">
+                                  <button
+                                    type="submit"
+                                    className="login100-form-btn btn-primary"
+                                  >
+                                    Submit
+                                  </button>
+                                  {/* <ToastContainer /> */}
+                                </div>
+                                <div className="text-center mt-4">
+                                  <p className="text-dark mb-0">
+                                    Forgot It?
+                                    <Link
+                                      to={`/`}
+                                      className="text-primary ms-1"
                                     >
-                                      E-Mail
-                                    </label>
-                                    <Field
-                                      className="form-control"
-                                      name="email"
-                                      placeholder="Enter Your Email"
-                                      type="email"
-                                    />
-                                    <ErrorMessage
-                                      className="text-danger"
-                                      name="email"
-                                      component="div"
-                                    />
-                                  </div>
-                                  <div className="container-login100-form-btn">
-                                    <button
-                                      type="submit"
-                                      className="login100-form-btn btn-primary"
-                                    >
-                                      Submit
-                                    </button>
-                                    {/* <ToastContainer /> */}
-                                  </div>
-                                  <div className="text-center mt-4">
-                                    <p className="text-dark mb-0">
-                                      Forgot It?
-                                      <Link
-                                        to={`/`}
-                                        className="text-primary ms-1"
-                                      >
-                                        Back to Login
-                                      </Link>
-                                    </p>
-                                  </div>
+                                      Back to Login
+                                    </Link>
+                                  </p>
                                 </div>
                               </div>
-                            </Card.Body>
-                            <Card.Footer
-                              className=" text-end p-2 color-white"
-                              style={{
-                                background:
-                                  "linear-gradient(90deg, rgb(70 99 172) 0%, rgb(70 99 172) 91.71%)",
-                                color: "white",
-                                fontSize: "12px",
-                              }}
-                            >
-                              <span
-                                className=" "
-                                style={{ paddingRight: "20px" }}
-                              >
-                                SECURE WITH{" "}
-                                <strong className="  font-weight-bold">
-                                  2FA
-                                </strong>{" "}
-                                <i
-                                  className="fa fa-shield"
-                                  aria-hidden="true"
-                                ></i>
-                              </span>
-                            </Card.Footer>
-                          </Form>
-                        )}
-                      </Formik>
-                    </Col>
-                  </Row>
+                            </div>
+                          </Card.Body>
+                          <Card.Footer
+                            className=" text-end p-2 color-white btn-primary"
+
+                          >
+                            <span className=" " style={{ paddingRight: "20px" }}>
+                              SECURE WITH{" "}
+                              <strong className="  font-weight-bold">2FA</strong>{" "}
+                              <i className="fa fa-shield" aria-hidden="true"></i>
+                              <i className="fa fa-brands-google"></i>
+                              <i className="fa fa-brands-microsoft"></i>
+                            </span>
+                          </Card.Footer>
+                        </Form>
+                      )}
+                    </Formik>
+                  </Card.Body>
                 </div>
               </div>
             </div>
-          </div>
-        </Col>
-      </Row>
-    </div>
+
+          </Col >
+        </Row >
+      </div >
+    </>
   );
 }
